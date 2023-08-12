78c7887752cf82c33761a5c0f89092335ae1edb5
Aditya Raj <138895774+rajaditya2213@users.noreply.github.com>
2023-08-12 20:08:04 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict";  //treat all JS code as a newer version
+
+// alert(3+3)  //we are using node js , not browser
+
+
+console.log(10
+    +10)     //code readability should be high
+console.log("Hello Aditya")
+
+let name="Aditya"
+let age=20
+let isloggedIn=false
+let state
+
+
+/* primtive datatype */
+
+//number=> 2 to the power 53
+//bigint
+//string => ' ' or " "
+//boolean => true/false
+//null=>standalone value
+//undefiend=> 
+//symbol=>unique
+
+console.log(typeof "Aditya");
+console.log(typeof age)
+console.log(typeof isloggedIn);
+
+console.log(typeof null);  //object
+
+console.log(typeof undefined);   //undefiend
