64dd3befedc7a2845af4b815729935ad01869bfb
Aditya Raj <138895774+rajaditya2213@users.noreply.github.com>
2023-08-12 19:49:54 +0000

    variable js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId=12345
+let accountEmail="adityaraj2201arya@gmail.com"
+let accountPassword="12345678"
+accountCity="Jaipur"
+let accountState
+
+// accountId=2    not allowed
+// accountEmail="rgolu@gmail.com"   //allowed
+accountPassword="987654"           //allowed
+accountCity="Delhi"             //allowed
+
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+
+/* Prefer not to use var because of issues in block scope and functional scope*/
+
+/*shortcut-key */
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
